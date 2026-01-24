b93aef1bf6aebc67b8fdb3640576663e637798cc
bc-22 <chrisb2287@gmail.com>
2026-01-24 13:09:20 +0000

    Add BoseSoundTouchController component

---
+++ app/page.js
@@ -0,0 +1,9 @@
+'use client';
+
+import React, { useState, useEffect, useRef } from 'react';
+import { Volume2, VolumeX, Play, Pause, SkipBack, SkipForward, Heart, Radio, Settings, Wifi, WifiOff } from 'lucide-react';
+
+export default function BoseSoundTouchController() {
+  // Copy the entire artifact code here
+  // (All the code from the artifact I created)
+}
